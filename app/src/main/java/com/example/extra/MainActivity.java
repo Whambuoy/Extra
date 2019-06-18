8b53f8ff8e9fd15f15dc7ce8de55803bd0930376
Whambuoy <esther25maina@gmail.com>
2019-06-18 09:40:44 +0300

    PutExtra

--- app/src/main/java/com/example/extra/MainActivity.java
+++ app/src/main/java/com/example/extra/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.extra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText name;
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.edittext1);
+        next = findViewById(R.id.button1);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String myname = name.getText().toString();
+
+                Intent welcome = new Intent(getApplicationContext(), Welcome.class);
+                welcome.putExtra("value", myname);
+
+                startActivity(welcome);
+            }
+        });
     }
 }
