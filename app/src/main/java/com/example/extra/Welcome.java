ccc56a023258b22be45361fd24cbfd1dace1109b
Whambuoy <esther25maina@gmail.com>
2019-06-18 09:55:12 +0300

    GetExtra

--- app/src/main/java/com/example/extra/Welcome.java
+++ app/src/main/java/com/example/extra/Welcome.java
@@ -1,13 +1,23 @@
 package com.example.extra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
 
+    TextView message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        message = findViewById(R.id.textview1);
+
+        Intent i = getIntent();
+        String welcome = i.getStringExtra("value");
+
+        message.setText(welcome);
     }
 }
